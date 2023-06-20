0a26b47edf4ad74d1088a9d5ddc9bc023fe3b9c9
maheshkh93 <maheshkh93@gmail.com>
2023-06-20 09:48:14 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+function setValues() {
+  let val = document.getElementsById("value1");
+  let maxval = document.getElementsById("value2");
+
+  alert("Input value should be less than Max value");
+}
