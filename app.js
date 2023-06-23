7f4548b2d062e0d3a7cfcbfdb27d2eb443b3af12
maheshkh93 <maheshkh93@gmail.com>
2023-06-23 08:47:58 +0530

    functionality updated

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 function setValues() {
-  let val = document.getElementsById("value1");
-  let maxval = document.getElementsById("value2");
-
-  alert("Input value should be less than Max value");
+  let z = 0;
+  let x = document.getElementById("value1");
+  let y = document.getElementById("value2");
+  let fill = document.querySelector(":root");
+  let percentage = document.getElementsByClassName("percentage");
+  if (x.value * 1 < y.value * 1) {
+    alert("ERROR : Input value is exceeding Max Value");
+  } else {
+    z = (y.value / x.value) * 100;
+  }
+  fill.style.setProperty("--barPercentage", `${100 - z}%`);
+  fill.style.setProperty("--circleAngle", `${(z / 100) * 360}deg`);
+  percentage[0].innerHTML = `${z.toPrecision(4)}%`;
+  percentage[1].innerHTML = `${z.toPrecision(4)}%`;
+  percentage[2].innerHTML = `${z.toPrecision(4)}%`;
 }
